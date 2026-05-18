7748bc12d0e3faf5448d2d6e45515d77848de90f
spela29 <cesacesare1@gmail.com>
2026-05-18 10:02:09 +0000

    update mr_interna.h con pipe

--- src/mr_internal.h
+++ src/mr_internal.h
@@ -4,7 +4,114 @@
 #include "mr.h"
 #include <threads.h>
 #include <stddef.h>
+#include <sys/types.h>
 
+/*
+questa struct detta la struttura interna di un'elaboriazione 
+(struct mr)
+*/
+struct mr
+{
+    mr_attr_t attr;//copia gli attributi di config
+    mr_mapper_t mapper;//callback mapper fornita dall'utente
+    mr_reducer_t reducer;//callback reducer fornita dall'utente
+    void* user_arg;// argomento opaco passato alle callback
+};
+
+
+//==========================SEZIONE-PIPE=========================================
+/*
+imposto i limiti ragionevoli per gli header passati su pipe 
+*/
+#define MR_MAX_TOKEN_LEN (1024) //1KB
+#define MR_MAX_VALUE_LEN (1024*1024) //1 MB
+#define MR_MAX_LINE_LEN (1024*1024)// 1MB
+#define MR_MAX_FNAME_LEN (4096) //4KB
+
+/*
+ora voglio definire delle struct che rapresentano gli header dei 
+messaggi scritti su pipe cosi che quando si va a leggere da pipe
+allora il proscesso che legge sa esattamente cosa legge e quanto 
+deve leggere
+*/
+
+/*
+struct che rappresenta l'header che va sulla pipe
+[principale -> mapper]
+*/
+typedef struct {
+    int           file_name_len;// lunghezza nome file, senza '\0' 
+    unsigned long line_number; //numero di riga
+    int           line_len;    //lunghezza contenuto riga, senza '\n' 
+} mr_line_header_t;
+
+
+/*
+struct che rappresenta l'header che va sulla pipe
+[mapper -> reducer]
+*/
+typedef struct {
+    int token_len;  //lunghezza token senza '\0' 
+    int value_len; //lunghezza in byte del val opaco nessun terminatore
+} mr_pair_header_t;
+
+/*
+struct che rappresenta l'header che va sulla pipe
+[reducer-> principale (risultato)]
+*/
+typedef struct {
+    int token_len;//lunghezza token senza '\0' 
+    int result_len;//lunghezza valore dopo la valutazione del reducer
+} mr_result_header_t;
+
+
+/*
+adesso voglio definire i prototipi delle funzioni che uso nel file sorgente 
+mr_pipe.c e voglio definire delle funzioni che fanno praticamente da wrapper
+per le fuznioni readn() e writen() cosi da sfruttare gli header appena definiti
+e le entita del mio framework che voglioni scrivere e leggere su pipe devono appoggiarsi
+e quidni chiamare queste funzioni wrapper che a sua volta useranno le readn() e writen()
+qundi facedo cosi si creano piu livelli di astrazione 
+*/
+
+/* 
+Livello basso 
+*/
+ssize_t readn(int fd, void *buf, size_t n);
+ssize_t writen(int fd, const void *buf, size_t n);
+
+/* 
+Principale -> Mapper 
+*/
+int mr_send_line(int fd,
+                 const char *file_name, size_t file_name_len,
+                 unsigned long line_number,
+                 const char *line, size_t line_len);
+int mr_recv_line(int fd, mr_line_header_t *hdr,
+                 char **file_name_out, char **line_out);
+
+/* 
+Mapper -> Reducer 
+*/
+int mr_send_pair(int fd,
+                 const char *token, size_t token_len,
+                 const void *value, size_t value_len);
+int mr_recv_pair(int fd,
+                 char **token_out, size_t *token_len_out,
+                 void **value_out, size_t *value_len_out);
+
+/* 
+Reducer -> Principale 
+*/
+int mr_send_result(int fd,
+                   const char *token, size_t token_len,
+                   const void *result, size_t result_len);
+int mr_recv_result(int fd,
+                   char **token_out,  size_t *token_len_out,
+                   void **result_out, size_t *result_len_out);
+
+
+//==========================SEZIONE-QUEUE=========================================
 /*
 questa struct mr_queue_info contiene la coda in items che è un array di puntatori
 e tutte le strutture necessarie per la gestione di essa.
@@ -21,6 +128,7 @@ typedef struct
     cnd_t not_empty;
     int  close; //indiacatore 0 default 1 = non arrivano piu elementi nella coda
 } mr_queue_info;
+
 
 /*
 questa funzione pernde in input il puntatore alla struct per la gestione della coda
---
+++ src/mr_pipe.c
@@ -0,0 +1,2 @@
+//scrivere mr_pipe.c 
+
